cf6350345baeaa3f85c470460b90573a8d6c2c32
Andre Dorr <89052086+andredorr@users.noreply.github.com>
2022-06-29 20:50:29 -0300

    ajustes

--- mail.py
+++ mail.py
@@ -1,3 +1,4 @@
+
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -28,13 +29,16 @@ def sendMail(mail):
     server.send_message(mail)
     server.quit()
 
-
-send_mail = input("Deseja enviar e-mail, digite 1 para envio?\n")
-if(send_mail == '1'):
-    SENDER_MAIL = input('Digite o seu email\n')
-    SENDER_PASS = input('Digite a sua senha\n')
-    toMail = input('Digite o email do destinatário\n')
-    subject = input('Digite o titulo do email\n')
-    body = input('Digite o conteudo do email\n')
-    mail = writeMail(toMail, subject, body, SENDER_MAIL)
-    sendMail(mail)
+send_mail = 0
+while send_mail != 2:
+    send_mail = int(input("Deseja enviar e-mail, digite 1 para envio?\n"))
+    if(send_mail == 1):
+        SENDER_MAIL = input('Digite o seu email\n')
+        SENDER_PASS = input('Digite a sua senha\n')
+        toMail = input('Digite o email do destinatário\n')
+        subject = input('Digite o titulo do email\n')
+        body = input('Digite o conteudo do email\n')
+        mail = writeMail(toMail, subject, body, SENDER_MAIL)
+        sendMail(mail)
+        send_mail = 2
+        print('Email enviado com sucesso!!!')
