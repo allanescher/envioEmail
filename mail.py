3dd3ffc18ba3e5dce0688435f4b54f8e68e2acf9
Andre Dorr <89052086+andredorr@users.noreply.github.com>
2022-06-29 20:35:40 -0300

    ajustes

--- mail.py
+++ mail.py
@@ -1,30 +1,26 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
-from email import encoders
 
-SENDER_MAIL = 'smtpbpk@outlook.com'
-SENDER_PASS = 'smtpbiopark01'
 
-SMTP_PROVIDER = [
-    'smtp.office365.com'
-]
+#SENDER_MAIL = 'smtpbpk@outlook.com'
+#SENDER_PASS = 'smtpbiopark01'
 
-def writeMail(to, subject, body):
+SMTP_PROVIDER = 'smtp.office365.com'
+
+
+def writeMail(to, subject, body, email):
     msg = MIMEMultipart()
-    msg['From'] = 'smtpbpk@outlook.com'
+    msg['From'] = email
     msg['To'] = to
     msg['Subject'] = subject
     body = body
     msg.attach(MIMEText(body, 'plain'))
-    #msg.attach(MIMEText(body, 'html'))
 
     return msg
 
-def sendMail(mail, prov):
-    p = SMTP_PROVIDER[0] if prov == 'outlook' else SMTP_PROVIDER[1]
-    server = smtplib.SMTP(p, 587)  ### put your relevant SMTP here
+def sendMail(mail):
+    server = smtplib.SMTP(SMTP_PROVIDER, 587)  ### put your relevant SMTP here
     server.ehlo()
     server.starttls()
     server.ehlo()
@@ -35,15 +31,10 @@ def sendMail(mail, prov):
 
 send_mail = input("Deseja enviar e-mail, digite 1 para envio?\n")
 if(send_mail == '1'):
-    toMail = "rodolfo_xz@hotmail.com"  #input('Digite seu email\n')
-#    body = """\
-#        <table>
-#            <tr>
-#                <th>Título</th><th>Ano</th><th>Metascore</th><th>User score</th><th># votos</th>
-#            </tr>
-#        """
-    body = "Aqui vai seu conteúdo\n\n"
-    count = 1
-    body += "Parabéns, você conseguiu enviar e-mail."
-    mail = writeMail(toMail, 'Envio e-mail', body)
-    sendMail(mail, 'outlook')
+    SENDER_MAIL = input('Digite o seu email\n')
+    SENDER_PASS = input('Digite a sua senha\n')
+    toMail = input('Digite o email do destinatário\n')
+    subject = input('Digite o titulo do email\n')
+    body = input('Digite o conteudo do email\n')
+    mail = writeMail(toMail, subject, body, SENDER_MAIL)
+    sendMail(mail)
